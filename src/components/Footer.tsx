ee640a62b91e07e18fca0e3271038fe9f93f7db0
Demohstens <demohstens@gmail.com>
2025-01-21 19:35:56 +0100

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
     return <> 
-        <div className="relative text-center bg-grey-background p-4 bottom-0 w-full h-[4rem]  ">
+        <div className="text-center bg-grey-background p-4 bottom-0 w-full h-[4rem] mt-auto ">
             <div className="links flex justify-around flex-wrap">
                 <a href="/Impressum" className="hover:text-primary text-black">Impressum</a>
                 <a href="/Datenschutz" className="hover:text-primary text-black">Datenschutz</a>
